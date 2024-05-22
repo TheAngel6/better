8dc3e9e48d46f3d106a65c95ac0c5355693d58a8
TheAngel <139715277+TheAngel6@users.noreply.github.com>
2024-05-22 09:31:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 import json
 import os
 
+# Read the webhook URL from an environment variable
 webhook_url = os.getenv('WEBHOOK_URL')
 
 def skicka_discord_meddelande(webhook_url, meddelande):
@@ -12,12 +13,17 @@ def skicka_discord_meddelande(webhook_url, meddelande):
         "Content-Type": "application/json"
     }
     response = requests.post(webhook_url, data=json.dumps(data), headers=headers)
-    if response.status_code == 200:
+    if response.status_code == 204:
         print("Meddelandet har skickats till Discord!")
     else:
-        print(f"Det uppstod ett fel: {response.status_code}")
-# Ersätt "din_webhook_url_här" med din faktiska webhook-URL
+        print(f"Det uppstod ett fel: {response.status_code}, {response.text}")
 
-webhook_url = "WEBHOOK_URL"
+# Ensure the webhook URL is set correctly
+if not webhook_url:
+    raise ValueError("WEBHOOK_URL is not set")
+
+# Example message
 meddelande = "Johannes>theo!"
+
+# Send the message to the Discord webhook
 skicka_discord_meddelande(webhook_url, meddelande)
