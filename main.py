efcb6a04fe219109e98892ca28e3a84d2f492b27
TheAngel <139715277+TheAngel6@users.noreply.github.com>
2024-07-25 22:31:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -20,6 +20,6 @@ def skicka_discord_meddelande(webhook_url, meddelande):
 if not webhook_url:
     raise ValueError("WEBHOOK_URL is not set")
 
-meddelande = "UwU! https://tenor.com/ncABPViuNYN.gif"
+meddelande = "skill issue! https://tenor.com/ncABPViuNYN.gif"
 
 skicka_discord_meddelande(webhook_url, meddelande)
