42a00898aee6829aae4e67136a82b616a95d8b61
TheAngel <139715277+TheAngel6@users.noreply.github.com>
2024-11-27 19:45:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ if not webhook_url:
     raise ValueError("WEBHOOK_URL is not set")
 
 # Define the message you want to send
-meddelande = "beep... boop... beta... boop... brawly... https://discord.com/oauth2/authorize?client_id=1304872602501648414"
+meddelande = "beep... boop... beta... boop... brawly..."
 
 # Call the function to send the message
 skicka_discord_meddelande(webhook_url, meddelande)
